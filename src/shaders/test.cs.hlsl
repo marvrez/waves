8b1d9c8529a79d374c3d3f2e50294fcfca8a50ff
Marvin Reza <marvin.reza@cognitedata.com@Marvin-sin-MBP.home>
2024-08-24 00:30:36 +0200

    chore: remove unused shaders

--- src/shaders/test.cs.hlsl
+++
@@ -1,8 +0,0 @@
-[[vk::binding(0, 0)]] StructuredBuffer<int> InputBuffer;
-[[vk::binding(1, 0)]] RWStructuredBuffer<int> OutputBuffer;
-
-[numthreads(1, 1, 1)]
-void main(uint3 gIdx : SV_DispatchThreadID)
-{
-    OutputBuffer[gIdx.x] = InputBuffer[gIdx.x] + 1;
-}
--- src/shaders/triangle.ps.hlsl
+++
@@ -1,4 +0,0 @@
-float4 main(float3 color : COLOR0) : SV_TARGET
-{
-    return float4(color, 1.0);
-}
