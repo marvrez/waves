2ca9f807f8afcbfad861c9d7a0694f8b21e69853
Marvin Reza <marvin.reza@cognitedata.com@Mac.home>
2025-01-05 02:29:35 +0100

    fix: tweak tools.h

--- src/shaders/tools.h
+++ src/shaders/tools.h
@@ -11,7 +11,7 @@ struct TilesCounter {
 struct IndirectDispatchArgs {
     // (2, 2 * numActiveTiles, 2) thread groups
     // We multiply by 2 since all work groups are dispatched with 8x8x8 threads;
-    // ideally, we want 16x16x16, but due to the 1024 workg group invocations
+    // ideally, we want 16x16x16, but due to the 1024 work group invocations
     // limit, we can't do that). As such, we need to dispatch twice as many work groups.
     uint3 gridGroupCount;         
     // (1, numActiveTiles, 1) thread groups.
@@ -24,14 +24,14 @@ struct TilePositionData {
     float3 cellPosition; // The position of the center of the cell in world space.
 };
 
-static inline int3 GetTileIndex(uint tileAddress)
+static inline int3 GetGridIndex(uint tileAddress)
 {
-    return int3(tileAddress & 1023, (tileAddress >> 10) & 1023, (tileAddress >> 20) & 1023);
+    return int3(tileAddress & 0xFF, (tileAddress >> 8) & 0xFF, (tileAddress >> 16) & 0xFF);
 }
 
 static inline uint GetTileAddress(int3 tileIndex)
 {
-    return tileIndex.x + (tileIndex.y << 10) + (tileIndex.z << 20);
+    return tileIndex.x + (tileIndex.y << 8) + (tileIndex.z << 16);
 }
 
 // Converts the 1D tile index (max value 4096) to a 3D tile index.
@@ -56,10 +56,10 @@ static inline TilePositionData GetTilePositionData(uint tile, uint tileAddress, 
     TilePositionData output = (TilePositionData)0;
 
     const int3 localOffset = int3(threadId.x & 0xF, threadId.y & 0xF, threadId.z & 0xF);
-    const int3 tileIdx = GetTileIndex(tileAddress);
+    const int3 gridIdx = GetGridIndex(tileAddress); // The base / start of the tile in the 3D grid.
 
     output.index = 16 * int3(GetPackedTile(tile).xyz) + localOffset;
-    output.cellPosition = float3(16.0 * tileIdx + localOffset) / 256.0;
+    output.cellPosition = float3(16.0 * gridIdx + localOffset) / 256.0;
 
     return output;
 }
